22109fcfe3612bc1d1e2d61fd1c7d5aca71fa234
Furkan Paşaoğlu <furkan.pasaoglu99@gmail.com>
2021-05-02 14:48:28 +0300

    4.Gün Ödev 2

--- interfaceAbstractDemo/src/Main.java
+++ interfaceAbstractDemo/src/Main.java
@@ -9,7 +9,16 @@ public class Main {
         customer1.setLastName("Demiroğ");
         customer1.setNationalityId("28861499108");
 
-	    BaseCustomerManager customerManager = new NeroCustomerManager(new CustomerCheckManager());
-	    customerManager.save(customer1);
+	Customer customer2 = new Customer();
+        customer2.setDateOfBirth(new Date(1999,8,6));
+        customer2.setFirstName("Furkan");
+        customer2.setLastName("Paşaoğlu");
+        customer2.setNationalityId("12345678910");
+
+	BaseCustomerManager customerManager1 = new NeroCustomerManager(new CustomerCheckManager());
+	customerManager1.save(customer1);
+	    
+	BaseCustomerManager customerManager2 = new StarbucksCustomerManager(new CustomerCheckManager());
+	customerManager2.save(customer2);
     }
 }
